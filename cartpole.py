668ceb3a25cdfac838c16973de1a3e5925940c20
Trusty Coder <trustycoder83@gmail.com>
2020-08-08 01:29:45 +0300

    Update cartpole.py

--- cartpole.py
+++ cartpole.py
@@ -6,55 +6,35 @@ from brain import Brain
 
 
 if __name__ == "__main__":
-    # Flag used to enable or disable screen recording
     recording_is_enabled = True
-
-    # Initializes the environment
     env = gym.make('CartPole-v1')
 
-    # Records the environment
     if recording_is_enabled:
         env = gym.wrappers.Monitor(env, "recording", video_callable=lambda episode_id: True, force=True)
 
-    # Defines training related constants
     batch_size = 32
     num_episodes = 1000
     num_episode_steps = 500
     action_size = env.action_space.n
     state_size = env.observation_space.shape[0]
 
-    # Creates the brain
     brain = Brain(state_size=state_size, action_size=action_size)
 
     for episode in range(num_episodes):
-        # Defines the total reward per episode
         total_reward = 0
-
-        # Resets the environment
         observation = env.reset()
-
-        # Gets the current state
         current_state = np.reshape(observation, [1, state_size])
 
         for episode_step in range(num_episode_steps):
-            # Renders the screen after new environment observation
             env.render(mode="human")
-
-            # Gets a new action
             action = brain.act(current_state)
 
-            # Takes action and calculates the total reward
             observation, reward, done, _ = env.step(action)
             reward = reward if not done else -10
             total_reward += reward
 
-            # Gets the next state
             next_state = np.reshape(observation, [1, state_size])
-
-            # Memorizes the experience
             brain.memorize(current_state, action, reward, next_state, done)
-
-            # Updates the current state
             current_state = next_state
 
             if done:
@@ -69,5 +49,4 @@ if __name__ == "__main__":
             if len(brain.memory) > batch_size:
                 brain.replay(batch_size)
 
-    # Closes the environment
     env.close()
